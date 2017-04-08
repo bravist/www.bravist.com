a458d182b48cfa81841af8db0e417810d4a89674
chenghuiyong <chenghuiyong@weipei.cc>
2017-04-08 22:27:45 +0800

    composer require laravel/passport && php artisan migrate && php artisan passport install

--- config/app.php
+++ config/app.php
@@ -176,7 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        /**
+         * Third-party Service Providers...
+         */
+        Laravel\Passport\PassportServiceProvider::class
     ],
 
     /*
