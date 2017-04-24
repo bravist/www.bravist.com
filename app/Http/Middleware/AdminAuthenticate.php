2dcca61ace4bd17b28ab78d5d02f5d0a8f5a09a1
chenghuiyong <chenghuiyong@weipei.cc>
2017-04-24 21:45:40 +0800

    Admin Authenticate

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Factory as AuthFactory;
+
+class AdminAuthenticate
+{
+    /**
+     * The guard factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @return void
+     */
+    public function __construct(AuthFactory $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (!$this->auth->guard($guard)->check()) {
+            return redirect('/admin/login');
+        }
+
+        return $next($request);
+    }
+}
